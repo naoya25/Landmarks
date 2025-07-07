a1f19204a8f68d6b5568abc0ea7838e905089e2d
naoya25 <onaoya2002@gmail.com>
2025-07-07 19:44:32 +0900

    feat part1

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,24 +1,39 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by 大塚直哉 on 2025/07/07.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    VStack {
+      MapView()
+        .frame(height: 300)
+
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+
+      VStack(alignment: .leading) {
+        Text("Turtle Rock")
+          .font(.title)
+        HStack {
+          Text("Joshua Tree National Park")
+          Spacer()
+          Text("California")
         }
-        .padding()
+        .font(.subheadline)
+        .foregroundStyle(.secondary)
+
+        Divider()
+
+        Text("About Turtle Rock")
+          .font(.title2)
+        Text("Descriptive text goes here.")
+      }
+      .padding()
+
+      Spacer()
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,10 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by 大塚直哉 on 2025/07/07.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
